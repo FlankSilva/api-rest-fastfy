acc8e916dfee1b16ac8509f1f470c0be9b8eef73
@flank.silva <flank.silva@cenariocapital.com.br>
2023-05-15 12:21:42 -0300

    create api-rest

---
+++ README.md
@@ -0,0 +1,42 @@
+## 💻 Sobre o projeto
+
+API-REST contruida a partir do framework Fastify Node, exemplificando um projeto backend com requisições HTTP.
+
+A plicação foi criada usando o typeScript e SQLite3, permitindo o teste com banco de dados local.
+
+## 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- [Node.js] [https://nodejs.org/en/]
+- [Fastify] [https://www.fastify.io/]
+- [TypeScript] [https://www.typescriptlang.org/]
+- [KnexJS] [https://knexjs.org/]
+
+
+## 🚀 Como executar o projeto
+
+### Pré-requisitos
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[Git](https://git-scm.com), [Node.js][nodejs]. 
+Além disto é bom ter um editor para trabalhar com o código como [VSCode][vscode]
+
+
+### 🎲 Rodando o Back End (servidor)
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/FlankSilva/api-rest-fastfy
+
+# Instale as dependências
+$ node install
+
+# Criando as tabelas do banco
+$ npm run knex -- migrate:latest
+
+# Execute a aplicação em modo de desenvolvimento
+$ node run dev
+
+# O servidor inciará na porta:3333 - acesse http://localhost:3333 
+```
---
+++ test/transactions.spec.ts
@@ -0,0 +1,120 @@
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process' // Permite executar comando no terminal
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  // Executa uma função antes dos testes. No caso, aguarde o servidor rodar
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  // Executa uma função depois dos testes. No cado feche a aplicação
+  afterAll(async () => {
+    await app.close()
+  })
+
+  // Executa algo, antes de cada teste. No caso estamos deletando todas as tabela do banco e criando novamente
+  beforeEach(() => {
+    execSync('npm run knex -- migrate:rollback --all')
+    execSync('npm run knex -- migrate:latest')
+  })
+
+  // Deve ser possivel criar uma transação
+  it('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  // Deve ser possivel lista uma transação
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+
+  it('should be able to get a specific transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionsResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionsResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
+})
